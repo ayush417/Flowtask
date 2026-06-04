9726f2d24f59093ce6168b3280bd720c18f5cf31
Ayush Joshi <ayushjoshi759@gmail.com>
2026-06-04 13:45:16 +0530

    Fix the middleware layer

--- flowtask/Backend/src/routes/taskroutes.js
+++ flowtask/Backend/src/routes/taskroutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const verifyToken = require('../middleware/authMiddleware')
+const verifyToken = require('../middleware/authmiddleware')
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskcontroller')
 
 router.get('/', verifyToken, getTasks)
